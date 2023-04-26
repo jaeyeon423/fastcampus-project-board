6c291d9684aad3b949c4f52aa3d2aa182e3cef6f
jaeyeon <jaeyeon423@gmail.com>
2023-04-26 16:53:24 +0900

    Article Service 골격 작성

---
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/service/ArticleService.java
@@ -0,0 +1,23 @@
+package com.fastcampus.projectboard.service;
+
+import com.fastcampus.projectboard.domain.Article;
+import com.fastcampus.projectboard.domain.type.SearchType;
+import com.fastcampus.projectboard.dto.ArticleDto;
+import com.fastcampus.projectboard.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class ArticleService {
+
+    private final ArticleRepository articleRepository;
+
+    public Page<ArticleDto> searchArticles(SearchType title, String search_keyword){
+        return Page.empty();
+    }
+
+}
