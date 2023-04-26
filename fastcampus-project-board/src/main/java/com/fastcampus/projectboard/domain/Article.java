1c2863403a68af71729aa5093529cfa6d1b4929e
jaeyeon <jaeyeon423@gmail.com>
2023-04-26 13:01:00 +0900

    도메인 -> 엔티티

--- fastcampus-project-board/src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -1,15 +1,65 @@
 package com.fastcampus.projectboard.domain;
 
-import java.time.LocalDateTime;
+import lombok.*;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Entity
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "title"),
+        @Index(columnList = "content"),
+        @Index(columnList = "createdAt"),
+        @Index(columnList = "createdBy")
+})
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Article {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Setter @Column(nullable = false)
     private String title;
+    @Setter @Column(nullable = false, length = 10000)
     private String content;
+    @Setter
     private String hashtag;
 
+    @CreatedDate @Column(nullable = false)
     private LocalDateTime createdAt;
+    @CreatedBy @Column(nullable = false, length = 100)
     private String createdBy;
+    @LastModifiedDate @Column(nullable = false)
     private LocalDateTime modifiedAt;
-    private LocalDateTime modifiedBy;
+    @LastModifiedBy @Column(nullable = false, length = 100)
+    private String modifiedBy;
+
+    private Article(String title, String content, String hashtag) {
+        this.title = title;
+        this.content = content;
+        this.hashtag = hashtag;
+    }
+
+    public static Article of(String title, String content, String hashtag){
+        return new Article(title, content, hashtag);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Article article)) return false;
+        return id != null && Objects.equals(id, article.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
